b3c8df415ab43563a940e0a11e9972d3c553705f
abrrar <96829545+abrrar@users.noreply.github.com>
2022-10-24 17:47:45 +0300

    Add files via upload

---
+++ HugPetApplications2/HugPetApplications2/HugPetApplications2App.swift
@@ -0,0 +1,17 @@
+//
+//  HugPetApplications2App.swift
+//  HugPetApplications2
+//
+//  Created by ABRAR on 28/03/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HugPetApplications2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
